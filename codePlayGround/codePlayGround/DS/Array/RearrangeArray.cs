c1ce5651f5f9a069459edfba4f43b49f8b7c066e
joseph <jktusoy@gmail.com>
2018-07-10 00:43:25 -0500

    3. Rearrange Array
    3. Rearrange Array

---
+++ codePlayGround/codePlayGround/DS/Array/RearrangeArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codePlayGround.DS.Array
+{
+    //https://www.geeksforgeeks.org/rearrange-array-arri/
+
+    class RearrangeArray
+    {
+
+
+        /*
+             int[] input1 = { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 };
+                int[] input2 = {19, 7, 0, 3, 18, 15, 12, 6, 1, 8, 11, 10, 9, 5, 13, 16, 2, 14, 17, 4};
+                print(ArrRearrange(input1));
+                print(ArrRearrange(input2));
+                Console.Read();
+         */
+
+
+        public static void print(int[] input)
+        {
+            Console.WriteLine(String.Join(",", input));
+        }
+        public static int[] ArrRearrange(int[] input)
+        {
+            int[] result = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (DoExist(i, input))
+                    result[i] = i;
+                else
+                    result[i] = -1;
+            }
+            return result;
+        }
+
+        public static bool DoExist(int find, int[] input)
+        {
+            bool result = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == find)
+                    return true;
+            }
+            return result;
+        }
+    }
+
+}
--- codePlayGround/codePlayGround/Program.cs
+++ codePlayGround/codePlayGround/Program.cs
@@ -6,23 +6,10 @@ namespace codePlayGround
     {
         static void Main(string[] args)
         {
-            int[] input = { -1, -2,0,9999};
-            Console.WriteLine(solution(input));
-            Console.Read();
-  
+        
         }
 
-
-        public static int solution(int[] A)
-        {
-            Array.Sort(A);
-            int result =1;
  
-            foreach (int i in A)
-                if (i == result) result++;
- 
-            return result;
-        }
 
 
     }
